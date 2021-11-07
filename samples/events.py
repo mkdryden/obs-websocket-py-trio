97d52bc513e5c7b5c0d225983e4fc1ecdac2e435
Michael D. M. Dryden <mk.dryden@utoronto.ca>
2021-11-07 02:06:30 -0500

    Update examples

--- samples/events.py
+++ samples/events.py
@@ -2,38 +2,36 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import time
-
 import logging
+
+import trio
+import typer
+
 logging.basicConfig(level=logging.INFO)
 
 sys.path.append('../')
-from obswebsocket import obsws, events  # noqa: E402
-
-host = "localhost"
-port = 4444
-password = "secret"
+from obswebsocket import open_obs_websocket, events  # noqa: E402
 
 
 def on_event(message):
-    print(u"Got message: {}".format(message))
+    print(f"Got message: {message}")
 
 
 def on_switch(message):
-    print(u"You changed the scene to {}".format(message.getSceneName()))
+    print(f"You changed the scene to {message.getSceneName()}")
 
 
-ws = obsws(host, port, password)
-ws.register(on_event)
-ws.register(on_switch, events.SwitchScenes)
-ws.connect()
+async def main(host: str = 'localhost', port: int = 4444, password: str = 'secret'):
+    async with open_obs_websocket(host, port, password) as ws:
+        ws.register(on_event)
+        ws.register(on_switch, events.SwitchScenes)
+        while True:
+            await trio.sleep_forever()
 
-try:
-    print("OK")
-    time.sleep(10)
-    print("END")
 
-except KeyboardInterrupt:
-    pass
+def cli_args(host: str = 'localhost', port: int = 4444, password: str = 'secret'):
+    trio.run(main, host, port, password)
 
-ws.disconnect()
+
+if __name__ == '__main__':
+    typer.run(cli_args)
--- samples/switch_scenes.py
+++ samples/switch_scenes.py
@@ -2,33 +2,32 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import time
-
 import logging
+
+import trio
+import typer
+
 logging.basicConfig(level=logging.INFO)
 
 sys.path.append('../')
-from obswebsocket import obsws, requests  # noqa: E402
+from obswebsocket import open_obs_websocket, requests  # noqa: E402
 
 
-host = "localhost"
-port = 4444
-password = "secret"
+async def main(host: str = 'localhost', port: int = 4444, password: str = 'secret'):
+    async with open_obs_websocket(host, port, password) as ws:
+        scenes = await ws.call(requests.GetSceneList())
+        for s in scenes.getScenes():
+            name = s['name']
+            print(f"Switching to {name}")
+            await ws.call(requests.SetCurrentScene(name))
+            await trio.sleep(2)
 
-ws = obsws(host, port, password)
-ws.connect()
+        print('End of list')
 
-try:
-    scenes = ws.call(requests.GetSceneList())
-    for s in scenes.getScenes():
-        name = s['name']
-        print(u"Switching to {}".format(name))
-        ws.call(requests.SetCurrentScene(name))
-        time.sleep(2)
 
-    print("End of list")
+def cli_args(host: str = 'localhost', port: int = 4444, password: str = 'secret'):
+    trio.run(main, host, port, password)
 
-except KeyboardInterrupt:
-    pass
 
-ws.disconnect()
+if __name__ == '__main__':
+    typer.run(cli_args)
